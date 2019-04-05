e4037b361b2cd5e6854a635b6145dfd54344b4e9
Wilton Rodrigues <wiltonsr94@gmail.com>
2019-04-05 13:49:18 -0300

    Initial commit

---
+++ Plugin.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Kanboard\Plugin\TrelloJSON2Kanboard;
+
+use Kanboard\Core\Plugin\Base;
+use Kanboard\Core\Translator;
+
+class Plugin extends Base
+{
+    public function initialize()
+    {
+    }
+
+    public function onStartup()
+    {
+        Translator::load($this->languageModel->getCurrentLanguage(), __DIR__.'/Locale');
+    }
+
+    public function getPluginName()
+    {
+        return 'TrelloJSON2Kanboard';
+    }
+
+    public function getPluginDescription()
+    {
+        return t('Plugin for Importing Trello Projects from JSON Files to Kanboard.');
+    }
+
+    public function getPluginAuthor()
+    {
+        return 'Wilton Rodrigues';
+    }
+
+    public function getPluginVersion()
+    {
+        return '1.0.0';
+    }
+
+    public function getPluginHomepage()
+    {
+        return 'https://github.com/wiltonsr/TrelloJSON2Kanboard';
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,31 @@
+TrelloJSON2Kanboard
+==============================
+
+Plugin for Importing Trello Projects from JSON Files to Kanboard.
+
+Author
+------
+
+- Wilton Rodrigues
+- License MIT
+
+Requirements
+------------
+
+- Kanboard >= 1.0.35
+
+Installation
+------------
+
+You have the choice between 3 methods:
+
+1. Install the plugin from the Kanboard plugin manager in one click
+2. Download the zip file and decompress everything under the directory `plugins/TrelloJSON2Kanboard`
+3. Clone this repository into the folder `plugins/TrelloJSON2Kanboard`
+
+Note: Plugin folder is case-sensitive.
+
+Documentation
+-------------
+
+TODO.
